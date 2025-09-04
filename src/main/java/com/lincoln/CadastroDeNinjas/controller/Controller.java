36148c8011270f30ce6d0636d7fe99f2abd34fa0
Lincoln Santiago <incoln.ufgd@gmail.com>
2025-09-04 02:25:09 -0300

    Criação da Entity e BD do JPA

--- src/main/java/com/lincoln/CadastroDeNinjas/controller/Controller.java
+++ src/main/java/com/lincoln/CadastroDeNinjas/controller/Controller.java
@@ -1,4 +1,4 @@
-package com.lincoln.CadastroDeNinjas;
+package com.lincoln.CadastroDeNinjas.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
